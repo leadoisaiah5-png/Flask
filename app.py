7c49fe915f100d17e085e5a5bd3ccbd989cc9f2f
leadoisaiah5-png <leadoisaiah5@gmail.com>
2026-06-26 12:05:00 +0300

    signin codes

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import *
 import pymysql
+import pymysql.cursors
 
 # initialize the application
 app = Flask(__name__)
@@ -9,7 +10,7 @@ app = Flask(__name__)
 
 # define the function
 def signup () :
-    # get user inputs from the form 
+    # get user inputs from the form .
     username = request.form["username"]
     email = request.form["email"]
     password = request.form["password"]
@@ -17,13 +18,105 @@ def signup () :
 
     # connection to database
     connection = pymysql.connect(
-        host="root" ,
-        user ="localhost" ,
+        host="localhost" ,
+        user ="root" ,
+        password="" ,
+        database="modcomleado"
+    )  
+
+    # define the cursor 
+    cursor = connection.cursor()
+
+    # define sql to insert users 
+    sql ="insert into users (username, password, email, phone)values(%s,%s,%s,%s)"
+
+    # define your data coming from step 3
+    data =(username, password, email,phone)
+
+    # execute/run the  querry 
+    cursor.execute(sql,data)
+
+    # commit/ save changes 
+    connection.commit()
+
+    return jsonify({"message" : "user registered successfully"})
+
+
+
+# member signin/login 
+# define the route/endpoint 
+@app.route("/api/signin" , methods=["POST"])
+
+# define the function 
+def signin() :
+
+    # get user inputs from the form 
+    email = request.form["email"]
+    password = request.form["password"] 
+
+    # connection to database 
+    connection = pymysql.connect(
+        host="localhost" ,
+        user ="root" ,
         password="" ,
         database="modcomleado"
     )
 
+    # define the cursor 
+    cursor = connection.cursor( pymysql.cursors.DictCursor)
+
+    # define sql to select users 
+    sql = "select * from users where email = %s and password= %s"
+
+    # define your data 
+    # NB: its coming from step 3 
+    data = (email, password)
+
+    # execute/run querry 
+    cursor.execute(sql, data)
+
+    # wrong  email and password 
+    if cursor. rowcount ==0 :
+        return jsonify({"message" : "invalid email or password" })
+
+    # correct emaol and password 
+    if cursor. rowcount ==1 :
+        # fetch the user 
+        user =cursor.fetchone()
+        return jsonify({"message" : "login successfull", "user" : user})
+
     
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # run the application
 app.run(debug=True)
